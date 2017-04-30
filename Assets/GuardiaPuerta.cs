fb01ca54ee8e92fb7d2823f44302a03343313936
Cambiar Nombre <Cambiar correo>
2017-04-29 19:07:49 -0500

    Guardia de la Puerta
    se creo el enemigo y la bala del enemigo de la puerta

---
+++ Assets/GuardiaPuerta.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardiaPuerta : MonoBehaviour {
+    public GameObject _BalaGuardiaPuerta;
+    public float FrecuenciaDisparo = 1f;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Disparo",0,FrecuenciaDisparo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Disparo()
+    {
+        Instantiate(_BalaGuardiaPuerta, transform.position, transform.rotation);
+    }
+
+
+}
---
+++ Assets/ProjectilGuardiaPuerta.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilGuardiaPuerta : MonoBehaviour {
+
+    public float speedX = 3f;
+    public float speedY = 0f;
+    // crear la explosion
+    public GameObject _explosion;
+    public string targetTag;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovimientoProjectilGuardiaPuerta();
+	}
+
+    void MovimientoProjectilGuardiaPuerta(){
+        transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            Instantiate(_explosion,other.transform.position,transform.rotation);
+        }        
+    }
+
+}
